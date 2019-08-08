3245c07bed813932df2e602743227aab7b78c398
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-08-08 18:00:42 +0900

    SYNC

--- billTotal/billTotal.js
+++ billTotal/billTotal.js
@@ -1,6 +1,9 @@
 function billTotal(subtotal) {
-  // YOUR CODE HERE
-  return
+  let tip = subtotal * 0.15;
+  let tax = subtotal * 0.095;
+  
+  return subtotal + tip + tax;
 }
+
 
 billTotal(10.00); // => 12.45
