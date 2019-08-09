8e4b59f3b90f60664ae2644e7760db718f065ddb
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-08-09 17:50:01 +0900

    SYNC

--- hammingDistance/hammingDistance.js
+++ hammingDistance/hammingDistance.js
@@ -1,14 +1,23 @@
-/**pseudo code
+3/**pseudo code
  * 
  * -strArr 입력
  *  -hammingDistanceCount
  *  -두 str의 character들 비교
- *   -
+ *   -다르면 hammingDistanceCount +
+ *   -return hammingDistanceCount
  */
 
 function hammingDistance(strArr) {
-  
-  return
+  let hammingDistanceCount = 0;
+  let str1 = strArr[0];
+  let str2 = strArr[1];
+
+  for(let i = 0; i < str1.length; i++) {
+    if(str1[i] !== str2[i]) {
+      hammingDistanceCount++;
+    }
+  }
+  return hammingDistanceCount;
 }
 
 hammingDistance(["coder", "codec"]); // => 1
