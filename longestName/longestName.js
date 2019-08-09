9b80dba904f1b303e430471790159675d177d497
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-08-09 17:50:19 +0900

    SYNC

--- longestName/longestName.js
+++ longestName/longestName.js
@@ -5,9 +5,33 @@ var people = [
   {name: {first: "Lem", middle: "E.", last: "Tweakit"}, age: 45},
   {name: {first: "Louis", last: "Reasoner"}, age: 21}
 ];
+/**pseudo code
+ * 
+ * -people 입력
+ *  -map 활용
+ *  -reduce 활용
+ */
 
 function longestName(people) {
-  // TODO: Your code here
+  let getFullName = function(obj) {
+    if(!("middle" in obj.name)) {
+      return obj.name.first + " " + obj.name.last;
+    } else {
+      return obj.name.first + " " + obj.name.middle + " " + obj.name.last;
+    }
+  }
+  let findLongestName = function(longestName, eachName) {
+    if(longestName.length < eachName.length) {
+      return eachName;
+    } else {
+      return longestName;
+    }
+  }
+
+  let fullNameArr = people.map(getFullName);
+  let result = fullNameArr.reduce(findLongestName);
+
+  return result;
 }
 
 longestName(people); //"Alyssa P. Hacker"
