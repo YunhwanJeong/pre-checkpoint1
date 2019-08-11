c8f5abbfcd3bf39172f58bdf394daece9dc2be7c
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-08-12 00:12:30 +0900

    SYNC

--- offLineMinimum/offLineMinimum.js
+++ offLineMinimum/offLineMinimum.js
@@ -1,42 +1,66 @@
-/**psedo code
+/**pseudo code
  * 
  * -strArr 입력
- *  -빈 배열 생성해두기
- *  -숫자로 형변환
- *   -nan 만나면 그 전까지 가장 작은 수 push
- *   -return join
- *  
+ * 
+ *  -strArrClone
+ *  -빈 배열 2개(숫자만, 결과)
+ *  -key = "E"
+ *   -E 만나면 숫자만의 min을 result로 보냄
+ *   -아니면 숫자를 숫자만으로 보냄
  */
 
 function offLineMinimum(strArr) {
-  let result = [];
-  let toNum = (el) => Number(el);
-  let toNumArr = strArr.map(toNum);
-  let toFindSmallestNum = function(smallestNum, eachNum) {
-    if(smallestNum > eachNum) {
-      return eachNum;
+  let onlyNum = [];
+  let resultArr = [];
+  let key = "E";
+
+  for(let i = 0; i < strArr.length; i++) {
+    if(strArr[i] === key) {
+      let min = Math.min(...onlyNum);
+      resultArr.push(min);
+      onlyNum.splice(onlyNum.indexOf(min), 1);
     } else {
-      return smallestNum;
+      onlyNum.push(Number(strArr[i]));
     }
   }
-
-  for(let i = 0; i < toNumArr.length; i++) {
-    if(Number.isNaN(toNumArr[i])) {
-      let toFindSmallNumArr = toNumArr.slice(0, i);
-      let smallestNum = toFindSmallNumArr.reduce(toFindSmallestNum);
-      for(let num of toFindSmallNumArr) {
-        if(num === smallestNum) {
-          result.push(num);
-        }
-      }
-    }
-  }
-  return result.join(", ");
+  return resultArr.join(",");
 }
-
 
 offLineMinimum(["1","2","E","E","3"]); // => '1,2'
 offLineMinimum(["4","E","1","E","2","E","3","E"]); // => '4,1,2,3'
+
+
+
+
+
+// function offLineMinimum(strArr) {
+//   let result = [];
+//   let toNum = (el) => Number(el);
+//   let toNumArr = strArr.map(toNum);
+//   let toFindSmallestNum = function(smallestNum, eachNum) {
+//     if(smallestNum > eachNum) {
+//       return eachNum;
+//     } else {
+//       return smallestNum;
+//     }
+//   }
+
+//   for(let i = 0; i < toNumArr.length; i++) {
+//     if(Number.isNaN(toNumArr[i])) {
+//       let toFindSmallNumArr = toNumArr.slice(0, i);
+//       let smallestNum = toFindSmallNumArr.reduce(toFindSmallestNum);
+//       for(let num of toFindSmallNumArr) {
+//         if(num === smallestNum) {
+//           result.push(num);
+//         }
+//       }
+//     }
+//   }
+//   return result.join(", ");
+// }
+
+
+
 
 // console.log 1
 
