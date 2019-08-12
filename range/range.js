14857727ad1c4ae13a5146c38dc974bb5ee75cfe
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-08-12 11:09:43 +0900

    pre_34_정윤환 pre-checkpoint1 complete

--- range/range.js
+++ range/range.js
@@ -1,8 +1,17 @@
+//Array.from
 function range(start, end) {
-  // YOUR CODE HERE
-  return
+  const length = end - start;
+  return Array.from({length}, (_, i) => start + i);
 }
 
+//recursive way
+// function range(start, end) {
+//   if(start >= end) {
+//     return [];
+//   }
+
+//   return [start, ...range(start + 1, end)];
+// }
 range(0, 4); // => [0, 1, 2, 3]
 range(2, 7); // => [2, 3, 4, 5, 6]
 range(10, 10); // => []
